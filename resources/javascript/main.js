e49ad34deed9680e27daaad81182d5e492670c97
Penzragon <rifkyaliffa@gmail.com>
2021-03-01 15:11:21 +0700

    Add script on skill, have to change function name

---
+++ resources/javascript/main.js
@@ -0,0 +1,18 @@
+let fact = document.getElementById("fact");
+const bDay = "04-23-1999";
+const bDayDate = new Date(bDay);
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+setInterval(() => {
+    const now = new Date();
+    const difference = Math.floor((now.getTime() - bDayDate.getTime()) / 1000);
+    fact.innerText = intlNumberFormatter.format(difference);
+}, 1000);
+
+const changeSize = (x) => {
+    x.style.color = "#f05454";
+};
+
+const resetSize = (x) => {
+    x.style.color = "";
+};
