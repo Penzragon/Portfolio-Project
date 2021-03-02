f46f41f773725b3eac01c85e9685d709ef5feab3
Penzragon <rifkyaliffa@gmail.com>
2021-03-02 14:23:16 +0700

    Add contact page and add javascript to navbar

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -4,15 +4,15 @@ const bDayDate = new Date(bDay);
 const intlNumberFormatter = new Intl.NumberFormat("en-US");
 
 setInterval(() => {
-    const now = new Date();
-    const difference = Math.floor((now.getTime() - bDayDate.getTime()) / 1000);
-    fact.innerText = intlNumberFormatter.format(difference);
+	const now = new Date();
+	const difference = Math.floor((now.getTime() - bDayDate.getTime()) / 1000);
+	fact.innerText = intlNumberFormatter.format(difference);
 }, 1000);
 
-const changeSize = (x) => {
-    x.style.color = "#f05454";
+const changeColor = (x) => {
+	x.style.color = "#f05454";
 };
 
-const resetSize = (x) => {
-    x.style.color = "";
+const resetColor = (x) => {
+	x.style.color = "";
 };
